29c7b2bcb61371a6b56d8a36b0a7072931509f9c
Rodolfo Llinás <rllinasc@unal.edu.co>
2019-12-07 15:34:58 -0500

    fixing posts migrations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+    belongs_to :posts, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,39 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_30_192439) do
+ActiveRecord::Schema.define(version: 2019_12_07_203304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "plpgsql"
 
-  create_table "comments", force: :cascade do |t|
-    t.string "comment"
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "post_id"
     t.bigint "user_id"
-    t.index ["post_id"], name: "index_comments_on_post_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "friend_requests", force: :cascade do |t|
-    t.bigint "requester_id"
-    t.bigint "requestee_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["requestee_id", "requester_id"], name: "index_friend_requests_on_requestee_id_and_requester_id", unique: true
-    t.index ["requestee_id"], name: "index_friend_requests_on_requestee_id"
-    t.index ["requester_id"], name: "index_friend_requests_on_requester_id"
-  end
-
-  create_table "friendships", force: :cascade do |t|
-    t.integer "requester_id"
-    t.integer "requestee_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["requestee_id", "requester_id"], name: "index_friendships_on_requestee_id_and_requester_id", unique: true
-    t.index ["requester_id"], name: "index_friendships_on_requester_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,4 +37,5 @@ ActiveRecord::Schema.define(version: 2019_11_30_192439) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
